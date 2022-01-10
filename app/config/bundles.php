c83b48f5aa2e78673a0b6022aa85d58cf2131401
Anis Draganovic <anis00723@gmail.com>
2022-01-11 00:25:46 +0100

    Added reset password and easy admin

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
