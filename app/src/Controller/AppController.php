f1bcf0bd5983de9142fc87dbc3c22d5ff6b843fd
Anis Draganovic <anis00723@gmail.com>
2022-01-10 18:41:37 +0100

    Adding user login and logout

---
+++ app/src/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    #[Route("/homepage", name: "app_homepage")]
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+            'user' => $this->getUser(),
+        ]);
+    }
+}
